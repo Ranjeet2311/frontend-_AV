22059c4a6a85465a5f5fc2fb9a5c068ad6a85362
Ranjeet Kumar <ranjeetkumar2311@gmail.com>
2021-08-18 12:11:27 +0200

    Initial commit

--- app6.js
+++ app6.js
@@ -5,24 +5,52 @@ const number = document.querySelector(".number");
 
 const speed = 10;
 
+window.onscroll = function () {
+  scrollCounterDisplay();
+};
+
+const scrollCounterDisplay = () => {
+  if (document.documentElement.scrollTop > 1850) {
+    counters.forEach((counter) => {
+      const updateCount = () => {
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+
+        let inc = target / speed;
+
+        if (count < target) {
+          number.innerText = (count + inc - 0.1).toFixed(1);
+          setTimeout(updateCount, 1000);
+        } else {
+          count.innerText = target;
+        }
+      };
+
+      updateCount();
+    });
+  }
+};
+
+// console.log(myFunction);
+
 // ----
-counters.forEach((counter) => {
-  const updateCount = () => {
-    const target = +counter.getAttribute("data-target");
-    const count = +counter.innerText;
+// counters.forEach((counter) => {
+//   const updateCount = () => {
+//     const target = +counter.getAttribute("data-target");
+//     const count = +counter.innerText;
 
-    let inc = target / speed;
+//     let inc = target / speed;
 
-    if (count < target) {
-      number.innerText = (count + inc - 0.1).toFixed(1);
-      setTimeout(updateCount, 1000);
-    } else {
-      count.innerText = target;
-    }
-  };
+//     if (count < target) {
+//       number.innerText = (count + inc - 0.1).toFixed(1);
+//       setTimeout(updateCount, 1000);
+//     } else {
+//       count.innerText = target;
+//     }
+//   };
 
-  updateCount();
-});
+//   updateCount();
+// });
 
 // -----
 // console.log(ev);
